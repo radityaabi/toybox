56482a4a9919e1b26e4644712ab20e66d153db6f
radityaabi <radityaabiansyah@gmail.com>
2026-01-09 14:54:54 +0700

    feat: enhance database seeding and add new toy entries with improved schema

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,28 +2,46 @@ import { categories, toys } from "./data";
 import { prisma } from "../src/lib/prisma";
 
 async function main() {
-  console.log("Seeding database...");
-  for (const category of categories) {
-    await prisma.category.upsert({
-      where: { id: category.id },
-      update: {},
-      create: category,
-    });
-  }
+  console.log("🌱 Seeding database...");
 
-  for (const toy of toys) {
-    await prisma.toy.upsert({
-      where: { sku: toy.sku },
-      update: {},
-      create: toy,
-    });
-  }
-  console.log("Seeding completed.");
+  // 1️⃣ Seed Categories
+  const resultCategories = await prisma.category.createManyAndReturn({
+    data: categories,
+    select: { id: true, slug: true },
+  });
+  console.log(`${resultCategories}`);
+  console.log(`✅ Seeded ${resultCategories.length} categories.`);
+
+  // 2️⃣ Seed Toys
+  const toysWithCategoryId = toys.map((toy) => {
+    const category = resultCategories.find((c) => c.slug === toy.categorySlug);
+    if (!category) {
+      throw new Error(`Category with slug ${toy.categorySlug} not found`);
+    }
+    return {
+      sku: toy.sku,
+      name: toy.name,
+      slug: toy.slug,
+      categoryId: category.id,
+      brand: toy.brand,
+      price: toy.price,
+      ageRange: toy.ageRange,
+      imageUrl: toy.imageUrl,
+      description: toy.description,
+    };
+  });
+
+  await prisma.toy.createMany({
+    data: toysWithCategoryId,
+  });
+
+  console.log(`✅ Seeded ${toys.length} toys.`);
+  console.log("Toy data created");
 }
 
 main()
   .catch((e) => {
-    console.error(e);
+    console.error("❌ Seeding failed:", e);
     process.exit(1);
   })
   .finally(async () => {
